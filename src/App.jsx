d58236e7125a826d3d9e72ff1af9b6d99e682da3
Daniel Kimolo <dankimist@gmail.com>
2024-05-31 15:00:09 +0300

    Still setting up the dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -8,8 +8,20 @@ function App() {
   return (
     <>
       <Navbar />
-      <Sidebar />
-      <Dashboard />
+      <div className="flex pt-16 overflow-hidden bg-gray-50 dark:bg-gray-900">
+        <Sidebar />
+        <div
+          className="fixed inset-0 z-10 hidden bg-gray-900/50 dark:bg-gray-900/90"
+          id="sidebarBackdrop"
+        ></div>
+
+        <div
+          id="main-content"
+          className="relative w-full h-full overflow-y-auto bg-gray-50 lg:ml-64 dark:bg-gray-900"
+        >
+          <Dashboard />
+        </div>
+      </div>
     </>
   );
 }
